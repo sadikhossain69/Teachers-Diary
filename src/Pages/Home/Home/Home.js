1e530aff68af35e80efdbf63db6e5a479508508a
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-17 17:36:44 +0600

    banner section done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,28 @@
 import './Home.css'
 import React from 'react';
 import banner from '../../../Photos/banner.png'
+import Typed from 'react-typed';
 
 const Home = () => {
     return (
-        <div style={{backgroundImage: `url(${banner})`, backgroundSize: 'cover', height: '88vh'}} >
+        <div style={{backgroundImage: `url(${banner})`, backgroundSize: 'cover', height: '88vh', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}} >
             <div className='col-lg-6 col-md-12 col-sm-12' >
                 <div className='home-container'>
-                    <h2>This is Home</h2>
+                    <h4>
+                        Hello! I'm a <span style={{color: 'skyblue'}} >Teacher😊</span>!
+                    </h4>
+                    <h2>
+                        <Typed
+                            strings={['A Teacher Can Make You Success',
+                                      "You Can't Achieve Success Without Teacher",
+                                      "A Perfect Teacher Makes A Student Parfect"
+                                    ]}
+                            typeSpeed={50}
+                            backSpeed={50}
+                            loop
+                        />
+                    </h2>
+                    <button className='btn btn-primary' >See More</button>
                 </div>
             </div>
         </div>
