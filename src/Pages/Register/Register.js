a49e235c5a06913d2a3747f9f986221f0bc326f1
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-17 23:15:48 +0600

    some problem solves

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -3,16 +3,21 @@ import React, { useState } from 'react';
 import registerImg from '../../Photos/register.svg'
 
 const Register = () => {
+    
 
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
 
     const handleEmail = event => {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
 
     const handlePassword = event => {
-        console.log(event.target.value);
+        setPassword(event.target.value);
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
     }
 
     return (
@@ -20,7 +25,7 @@ const Register = () => {
             <div className='row'>
                 <div className='col-lg-6 col-md-6 col-sm-12 register-form mb-5 '>
                     <h2>Sign Up</h2>
-                    <form>
+                    <form onSubmit={handleSubmit} >
                         <div className="mb-3">
                             <input type="text" className="form-control" id="exampleName" aria-describedby="nameHelp" placeholder='Enter Name(Optional)' />
                         </div>
