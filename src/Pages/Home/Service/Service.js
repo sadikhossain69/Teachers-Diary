fdf4ed472f0011405a6cb6d2161d945709de8aad
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-17 21:03:50 +0600

    service item 50 successful

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,13 +1,21 @@
 import './Service.css'
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Service = (props) => {
-
+    console.log(props.service);
     const { name, image, description } = props.service
     
     return (
-        <div>
-            <h2>This is Service!!!! {name} </h2>
+        <div className='col-lg-4 col-md-6 col-sm-12 g-5 p-3' >
+            <div className="service-card">
+                <img style={{height: '80px'}} src={image} alt="" />
+                <h3>{name}</h3>
+                <p>
+                    {description}
+                </p>
+                <Link className='btn' to='/checkout' >Buy Now</Link>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -6,18 +6,25 @@ import service1 from '../../../Photos/service1.svg'
 const Services = () => {
 
     const services = [
-        {id: 1, name: 'Math Tuition', image: service1, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' }
+        {id: 1, name: 'Math Tuition', image: service1, price: 9.99, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' },
+        {id: 2, name: 'Math Tuition', image: service1, price: 9.99, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' },
+        {id: 3, name: 'Math Tuition', image: service1, price: 9.99, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' },
+        {id: 4, name: 'Math Tuition', image: service1, price: 9.99, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' },
+        {id: 5, name: 'Math Tuition', image: service1, price: 9.99, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' },
+        {id: 6, name: 'Math Tuition', image: service1, price: 9.99, description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit totam nisi excepturi sunt voluptates asperiores sequi beatae optio quam! Fugiat officia, maxime facere minus rerum porro quis praesentium provident in?' },
     ]
 
     return (
         <div className='mt-3'>
             <h2>My <span style={{color: 'skyblue'}}>Services</span></h2>
-            <div>
-                {
-                    services.map(service => <Service
-                        key={service.id} service={service}
-                    />)
-                }
+            <div className="container">
+                <div className='row'>
+                    {
+                        services.map(service => <Service
+                            key={service.id} service={service}
+                        />)
+                    }
+                </div>
             </div>
         </div>
     );
