7b7797faed8e95faeef2971a97748d979cde6fe4
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-17 17:02:57 +0600

    home component 50% done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,15 @@
 import './Home.css'
 import React from 'react';
+import banner from '../../../Photos/banner.png'
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home!!!</h2>
+        <div style={{backgroundImage: `url(${banner})`, backgroundSize: 'cover', height: '88vh'}} >
+            <div className='col-lg-6 col-md-12 col-sm-12' >
+                <div className='home-container'>
+                    <h2>This is Home</h2>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import teachersLogo from '../../../Photos/teachers-logo.png'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar-component">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link className="ms-5" to='/' ><img style={{height: '50px'}} src={teachersLogo} alt="" /></Link>
