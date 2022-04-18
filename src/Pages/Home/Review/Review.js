717d3d88904a55e63e4dd9eca3343ead47860f17
sadikhossain69 <sadikhossain433@gmail.com>
2022-04-18 20:19:20 +0600

    react reveal install successful

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -1,15 +1,18 @@
 import './Review.css'
 import React from 'react';
+import { Fade } from 'react-reveal';
 
 const Review = ({review}) => {
     const { name, rating, description, image } = review
     return (
-        <div className='col-lg-4 col-md-6 col-sm-12 mt-4' >
+        <Fade>
+            <div className='col-lg-4 col-md-6 col-sm-12 mt-4' >
             <img className='w-50' style={{borderRadius: '10px',}} src={image} alt="" />
             <h4>{name}</h4>
             <span>{rating}</span>
             <p> {description} </p>
         </div>
+        </Fade>
     );
 };
 
